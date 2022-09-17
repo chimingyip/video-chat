3638a728116aa65e9b900a43e5f6925743a043bb
Chi-Ming Yip <cmyip99@gmail.com>
2022-09-17 16:03:54 +1000

    Added chatroom view and redirect

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!');
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:chatroom', (req, res) => {
+  res.render('chatroom', { roomId: req.params.chatroom})
+})
+
+app.get('/chiming', (req, res) => {
+  res.send('Hello Chi Ming!');
 });
