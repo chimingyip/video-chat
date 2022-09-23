0b21de7adb8c47ee8bae5cbb8be6fbbe5bee31ef
Chi-Ming Yip <cmyip99@gmail.com>
2022-09-23 18:01:32 +1000

    WIP: fixing webcam stream bug

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
+// Load environment variables
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-const server = require('http').Server(app);
-
+const http = require('http');
+const { ExpressPeerServer } = require('peer');
+const server = http.createServer(app);
 const io = require('socket.io')(server);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.listen(port, () => {
+  console.log(`Running on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-  // res.send('Hello World!');
   res.redirect(`/${uuidv4()}`);
 });
 
 app.get('/:chatroom', (req, res) => {
   res.render('chatroom', { roomId: req.params.chatroom})
-})
-
-app.get('/chiming', (req, res) => {
-  res.send('Hello Chi Ming!');
 });
 
 io.on('connection', (socket) => {
+  // socket.on('join-room', (roomId, userId) => {
+  //   socket.join(roomId);
+  //   socket.to(roomId).broadcast.emit('user-connected', userId);
+  // });
   console.log('A user has connected.');
 });
